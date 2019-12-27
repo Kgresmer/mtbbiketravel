cf941e21bb175ab5a56641feedd4bcf1a5179b28
kgresmer <kgresmer@gmail.com>
2019-12-27 09:29:51 -0600

    Updating navbar to have less buttons for mobile version.

--- src/public/nav/nav.jsx
+++ src/public/nav/nav.jsx
@@ -6,6 +6,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 const Navigation = (props) => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
   const [route, setRoute] = useState('');
 
   useEffect(() => {
@@ -13,38 +14,80 @@ const Navigation = (props) => {
 
   }, [props.location]);
 
+  useEffect(() => {
+    if (!typeof window === 'object') {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const showNav = () => {
+    if (windowSize > 768) {
+      return (
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand className="image-link" href="/">
+            <img className='logo' src={headerImage} alt="Mountain Bike Travel logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <div className="justify-content-end navbar-collapse collapse show" id="basic-navbar-nav">
+              <ul className="main-nav nav">
+                <li className="nav-item">
+                  <Nav.Link href="/home#Overview" className={`${route === '/home#Overview' ? 'main-nav-active-link' : ''}`}>Overview</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/home#Itinerary" className={`${route === '/home#Itinerary' ? 'main-nav-active-link' : ''}`}>Itinerary</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/home#WhatsIncluded" className={`${route === '/home#WhatsIncluded' ? 'main-nav-active-link' : ''}`}>Whats Included</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/home#DatesPricing" className={`${route === '/home#DatesPricing' ? 'main-nav-active-link' : ''}`}>Dates & Pricing</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/gallery" className={`${route === '/gallery' ? 'main-nav-active-link' : ''}`}>Gallery</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/contact" className={`${route === '/contact' ? 'main-nav-active-link' : ''}`}>Contact</Nav.Link>
+                </li>
+              </ul>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    } else {
+      return (
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand className="image-link" href="/">
+            <img className='logo' src={headerImage} alt="Mountain Bike Travel logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <div className="justify-content-end navbar-collapse collapse show" id="basic-navbar-nav">
+              <ul className="main-nav nav">
+                <li className="nav-item">
+                  <Nav.Link href="/gallery" className={`${route === '/gallery' ? 'main-nav-active-link' : ''}`}>Gallery</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link href="/contact" className={`${route === '/contact' ? 'main-nav-active-link' : ''}`}>Contact</Nav.Link>
+                </li>
+              </ul>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+      )
+    }
+  };
+
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand className="image-link" href="/">
-          <img className='logo' src={headerImage} alt="Mountain Bike Travel logo"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <div className="justify-content-end navbar-collapse collapse show" id="basic-navbar-nav">
-            <ul className="main-nav nav">
-              <li className="nav-item">
-                <Nav.Link href="/home#Overview" className={`${route === '/home#Overview' ? 'main-nav-active-link' : ''}`}>Overview</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="/home#Itinerary" className={`${route === '/home#Itinerary' ? 'main-nav-active-link' : ''}`}>Itinerary</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="/home#WhatsIncluded" className={`${route === '/home#WhatsIncluded' ? 'main-nav-active-link' : ''}`}>Whats Included</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="/home#DatesPricing" className={`${route === '/home#DatesPricing' ? 'main-nav-active-link' : ''}`}>Dates & Pricing</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="/gallery" className={`${route === '/gallery' ? 'main-nav-active-link' : ''}`}>Gallery</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="/contact" className={`${route === '/contact' ? 'main-nav-active-link' : ''}`}>Contact</Nav.Link>
-              </li>
-            </ul>
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
+      {showNav()}
     </div>
   )
 };
